afe4941c84f45948806455cc080cd9897aa4e65f
Rémi Bruyère <remi.bruyere@affluences.com>
2020-06-30 22:07:53 +0200

    Add missing DTO

---
+++ src/main/java/com/cyphernet/api/account/model/AccountPasswordDTO.java
@@ -0,0 +1,8 @@
+package com.cyphernet.api.account.model;
+
+import lombok.Getter;
+
+@Getter
+public class AccountPasswordDTO {
+    private String password;
+}
---
+++ src/main/java/com/cyphernet/api/account/model/AccountUpdateDTO.java
@@ -0,0 +1,21 @@
+package com.cyphernet.api.account.model;
+
+import lombok.Data;
+import lombok.experimental.Accessors;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+
+@Data
+@Accessors(chain = true)
+public class AccountUpdateDTO {
+    @NotNull(message = "Please provide a email")
+    @Length(min = 1, message = "Please don't provide a empty email")
+    @Email(message = "Your email format is wrong !")
+    private String email;
+
+    @NotNull(message = "Please provide a username")
+    @Length(min = 1, message = "Please don't provide a empty username")
+    private String username;
+}
